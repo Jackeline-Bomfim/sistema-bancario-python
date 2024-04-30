1940f7b31424bb2b23c24e7b1809193cbc247802
Jackeline Bomfim <62723813+Jackeline-Bomfim@users.noreply.github.com>
2024-04-30 11:04:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ extrato = ""
 numero_de_saques = 0
 LIMITE_SAQUES = 3
 contagem = 0
+soma_de_saques_diarios = 0
 
 while True:
 
@@ -23,16 +24,23 @@ while True:
         if numero_de_saques < LIMITE_SAQUES:
             valor = float(input("Quanto você deseja sacar? "))
 
-            if valor <= saldo and valor <= 500:
+            controlador_de_limite = valor + soma_de_saques_diarios
+
+            if valor <= saldo and valor <= limite and controlador_de_limite <= limite:
                 contagem += 1
                 saldo -= valor
-                numero_de_saques =+ 1
+                numero_de_saques +=  1
+                soma_de_saques_diarios += valor
                 extrato += f"Saque: R$ {valor:.2f}\n"
                 print(f"Você sacou R$ {valor:.2f} seu saldo atual é R$ {saldo:.2f}")
+            elif valor > limite:
+                print(f"O valor solicitado é maior que o ilimite diário para saque. Atualmente seu limite diário é de {limite:.2f}")
+            elif controlador_de_limite >= limite:
+                print(f"Você atingiu o limite de R$ {limite:.2f} para saque diários. Você já sacou R$ {soma_de_saques_diarios:.2f} hoje.")
             else: 
                 print("Você não tem saldo suficiente para efetuar essa transação.")
         else:
-            print("Você atingiu o limite de 3 saques diários.")         
+            print("Você atingiu o limite de 3 saques diários.")    
 
        
 
@@ -59,9 +67,7 @@ while True:
             print("=======================================")
 
 
-    elif opcao == "4":
+    elif opcao == "0":
         break
     else:
         print("Opção inválida, tente novamente com uma opção válida.")
-
-
