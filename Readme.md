39d4ade28481475abcab804c3ec812cbcdb09e97
Jackeline Bomfim <jackeline.bomfim644@gmail.com>
2024-05-04 12:18:13 -0300

    Criação das funções de saldo, deposito e extrato

--- Readme.md
+++ Readme.md
@@ -16,3 +16,32 @@
 <p>Os valores devem ser exibidos utulizando o formado R$ XXX,XX, exemplo:
 
 1500.45 = R$ 1500.45</p>
+
+### Etapas do segundo desafio
+
+Precisamos deixar nosso cógido mais modularizado, para isso vamos criar funções para as operações existentes: sacar, depositar e visializar histórico. Além disso, para a versão 2 do nosso sistema precisamos criar duas novas funções: criar usuário (cliente do banco) e criar conta corrente (vincular com usuário).
+
+#### **Regras**
+
+* Separação em funções</br>
+  Devemos criar funções para todas as operações do sistema. Para exercitar tudo o que aprendemos neste módulo, cada função vai ter uma regra na passagem de argumentos. O retorno e a forma como serão chamadas, pode ser definida por você da forma que achar melhor.
+
+* Saque</br>
+  Deve receber os argumentos apenas por nome (keyword only). Sugestão de argumentos: Saldo, valor, extrato, limite, numero_saques, limite_saques. Sugestão de retorno: Saldo e extrado.
+
+* Depósito</br>
+  deve receber os argumentos apenas por posição (positional only). Sugestão de argumentos: saldo, valor, extrato. Sugestão de retorno: saldo e extrato.
+
+* Extrato</br>
+  Deve receber os argumentos por posição e nome (positional only e keyword only). Argumentos posicionais: saldo, argumentos nomeados: extrato.
+
+* Novas funções</br>
+  Criar usuário e criar conta corrente. Fique a vontade para adicionar mais funções, exemplo: listar contas.
+
+* Criar usuário (cliente)</br>
+  o programa deve armazenar os usuários em uma lista, um usuário é composto por: nome, data de nascimento, cpf e endereço. O endereço é uma string com o formato: logradouro, número - bairro - caidade/sigla estado. Deve ser armazenado somente os números do cpf. não podemos cadastrar 2 usuários com o mesmo cpf.
+
+* Criar conta corrente</br>
+  O programa deve armazenar contas em uma lista, uma conta é composta por: agência, número da conta e usuário. O número da conta é sequencial, iniciando em 1. O número da agência é fixo: "0001". O usuário pode ter mais de uma conta mais uma conta pertence a somente um usuário.
+
+**DICA**: para vincular um usuário a uma conta, filtre a lista de usuários buscando o número do cpf informado para cada usuário da lista.
--- main.py
+++ main.py
@@ -13,50 +13,90 @@ extrato = ""
 numero_de_saques = 0
 LIMITE_SAQUES = 3
 contagem = 0
+soma_de_saques_diarios = 0
+
+
+def sacar(*,saldo, valor, extrato, limite):
+    
+    global numero_de_saques
+    global soma_de_saques_diarios
+    global contagem
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor + soma_de_saques_diarios > limite
+    excedeu_saques = numero_de_saques >= LIMITE_SAQUES
+    
+    if excedeu_saques == False:        
+        if excedeu_limite:
+            print(f"Você atingiu o limite de R$ {limite:.2f} para saque diários. Você já sacou R$ {soma_de_saques_diarios:.2f} hoje.")
+        elif excedeu_saldo: 
+            print("Você não tem saldo suficiente para efetuar essa transação.")
+        elif valor > 0:
+            saldo -= valor
+            numero_de_saques += 1
+            soma_de_saques_diarios += valor
+            contagem += 1
+            extrato += f"Saque: \t \t R$ {valor:.2f}\n"
+            print(f"Você sacou R$ {valor:.2f} seu saldo atual é R$ {saldo:.2f}")
+    else:
+        print(f"Você atingiu o limite de {LIMITE_SAQUES} de saques diários")
+
+    return (saldo, extrato)
+
+
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        global contagem
+        contagem += 1
+        saldo += valor
+        extrato += f"Depósito: \t R$ {valor:.2f}\n"
+        print(f"Você depositou R$ {valor:.2f} seu saldo atual é R$ {saldo:.2f}")
+    else:
+        print("Erro com o valor, tente novamente com valores iguais ou superiores a R$ 1,00.")
+
+    return (saldo, extrato)
+
+def exibir_extrato(saldo,/, *, extrato):
+    global contagem
+    if contagem == 0:
+        print("*************** EXTRATO ***************")
+        print("Não foram realizadas transações hoje.")
+        print(f"Seu saldo atual é R$ {saldo:.2f}")
+        print("======================================")
+    elif contagem > 0:
+        print("*************** EXTRATO ***************")
+        print(extrato)
+        print(f"Saldo: \t \t R$ {saldo}")
+        print("=======================================")
+    return(saldo, extrato)
 
 while True:
 
     opcao = input(menu)
 
     if opcao == "1":
-
-        if numero_de_saques < LIMITE_SAQUES:
-            valor = float(input("Quanto você deseja sacar? "))
-
-            if valor <= saldo and valor <= 500:
-                contagem += 1
-                saldo -= valor
-                numero_de_saques =+ 1
-                extrato += f"Saque: R$ {valor:.2f}\n"
-                print(f"Você sacou R$ {valor:.2f} seu saldo atual é R$ {saldo:.2f}")
-            else: 
-                print("Você não tem saldo suficiente para efetuar essa transação.")
-        else:
-            print("Você atingiu o limite de 3 saques diários.")         
-
-       
+        valor = float(input("Qual o valor que deseja sacar: "))
+        saldo, extrato = sacar(
+            saldo=saldo,
+            valor=valor,
+            extrato=extrato,
+            limite=limite,
+        )          
+  
 
     elif opcao == "2":
         valor = float(input("Quanto você deseja depositar? "))
-        if valor > 0:
-            contagem += 1
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-            print(f"Você depositou R$ {valor:.2f} seu saldo atual é R$ {saldo:.2f}")
-        else:
-            print("Erro com o valor, tente novamente com valores iguais ou superiores a R$ 1,00.")
-
+        
+        saldo, extrato = depositar(
+            saldo,
+            valor,
+            extrato
+        )
        
     elif opcao == "3":
-        if contagem == 0:
-            print("*************** EXTRATO ***************")
-            print("Não foram realizadas transações hoje.")
-            print(f"Seu saldo atual é R$ {saldo:.2f}")
-            print("======================================")
-        elif contagem > 0:
-            print("*************** EXTRATO ***************")
-            print(extrato)
-            print("=======================================")
+        saldo, extrato = exibir_extrato(
+            saldo,
+            extrato=extrato
+        )
 
 
     elif opcao == "4":
